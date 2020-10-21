22496a516b46e98c94b88cb84fa2ff292710d694
Scott Lorberbaum <scott@laureltreecomputing.com>
2020-10-20 18:33:52 -0600

    added basebuffer.h and basebuffer.c to executable to later add to output file.  This is temporary, yet I did not want users to have to include a library with their code every time. I also didn't want to duplicate the buffer code in the output file and in the project. We'll see if this sticks.

--- src/outfile.s
+++ src/outfile.s
@@ -1,7 +1,21 @@
 	.global _blob
 	.global _blob_size
+	.global _bufferh
+	.global _bufferh_size
+	.global _bufferc
+	.global _bufferc_size
 _blob:
 	.incbin "outfile.in"
 1:
 _blob_size:
 	.int 1b - _blob
+_bufferh:
+	.incbin "basebuffer.h"
+2:
+_bufferh_size:
+	.int 1b- _bufferh
+_bufferc:
+	.incbin "basebuffer.c"
+3:
+_bufferc_size:
+	.int 1b- _bufferc
